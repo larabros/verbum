198837697c634fbca929bb55fa85506784e15b5f
Nic Gutierrez <nicmanzana@gmail.com>
2016-05-18 14:10:02 +0100

    whops.

--- src/helpers.php
+++ src/helpers.php
@@ -4,6 +4,6 @@ if (!function_exists('vox'))
     function vox($key)
     {
         $value = call_user_func_array([\Larabros\Verbum\Vox::$class, 'get'], [$key]);
-        var_dump($value);
+        echo $value;
     }
 }
