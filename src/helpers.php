79c4646bba74ccbccb0b5b9ac00896e5a0932bce
Nic Gutierrez <nicmanzana@gmail.com>
2016-05-19 14:45:46 +0100

    vox now returns the value instead of printing it

--- src/helpers.php
+++ src/helpers.php
@@ -4,6 +4,6 @@ if (!function_exists('vox'))
     function vox($key)
     {
         $value = call_user_func_array([\Larabros\Verbum\Vox::$class, 'get'], [$key]);
-        echo $value;
+        return $value;
     }
 }
